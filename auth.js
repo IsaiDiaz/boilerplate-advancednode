203b87e90efa1761af344fc1211d5b471fb2f339
José Diaz <>
2022-07-17 14:52:26 +0000

    Todos los ejercicios completados

--- auth.js
+++ auth.js
@@ -2,6 +2,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const ObjectID = require('mongodb').ObjectID;
+const GithubStrategy = require('passport-github').Strategy
+
+
 
 module.exports = function (app, myDataBase) {
     passport.serializeUser((user, done) => {
@@ -27,5 +30,36 @@ module.exports = function (app, myDataBase) {
                 return done(null, user);
             });
         }
+    ));
+    passport.use(new GithubStrategy({
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: 'https://boilerplate-advancednode-1.josediaz43.repl.co/auth/github/callback'
+    }, function (accesToken, refresgToken, profile, cb) {
+      console.log(profile);
+      myDataBase.findOneAndUpdate(
+        { id: profile.id},
+        {
+          $setOnInsert: {
+            id: profile.id,
+            name: profile.displayName  || 'John Doe',
+            photo: profile.photos[0].value || '',
+            email: Array.isArray(profile.emails) ? profile.emails[0].value : 'No public email',
+            created_on: new Date(),
+            provider: profile.provider || ''
+          },
+          $set: {
+            last_login: new Date()
+          },
+          $inc: {
+            login_count: 1
+          }
+        },
+        { upsert: true, new: true},
+        (err, doc) => {
+          return cb(null, doc.value);
+        }
+      );
+    }
     ));
 }
--- routes.js
+++ routes.js
@@ -2,20 +2,24 @@ const passport = require('passport');
 const bcrypt = require('bcrypt');
 
 module.exports = function (app, myDataBase) {
-    app.get('/', (req, res) => {
+    app.route('/').get((req, res) => {
         res.render('pug',
-            { title: 'Connected to Database', message: 'Please login', showLogin: true, showRegistration: true });
+            { title: 'Connected to Database', message: 'Please login', showLogin: true, showRegistration: true, showSocialAuth: true });
     });
 
-    app.post('/login', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
         res.redirect('/profile');
     });
 
-    app.get('/profile', ensureAuthenticated, (req, res) => {
-        res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
+    app.route('/profile').get(ensureAuthenticated, (req, res) => {
+        res.render('/pug/profile', { username: req.user.username });
     });
 
-    app.get('/logout', (req, res) => {
+  app.route('/chat').get(ensureAuthenticated, (req, res) => {
+    res.render('pug/chat', { user: req.user });
+  });
+  
+    app.route('/logout').get((req, res) => {
         req.logout();
         res.redirect('/');
     });
@@ -49,6 +53,14 @@ module.exports = function (app, myDataBase) {
         }
     );
 
+    app.route('/auth/github').get(passport.authenticate('github'));
+
+    app.route('/auth/github/callback').get(passport.authenticate('github', {failureRedirect: '/'}),(req, res) => {
+        req.session.user_id = req.user.id;
+        res.redirect('/chat');
+    });
+
+  
     app.use((req, res, next) => {
         res.status(404).type('text').send('Not Found');
     });
